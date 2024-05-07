4dc5fef9cb6792742060c3877196cf0f0b8f8d38
saffi <saffimaharjan@gmail.com>
2024-05-07 16:57:47 +0545

    addes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import { marked } from 'marked';
 
 function App() {
+  const [change, setChange] = useState(`
+  # H1
+## H2
+[title](https://www.google.com)
+\`code\`
+\`\`\`
+{
+  "name":"june"
+
+}
+\`\`\`
+
+> Block Quotes!
+
+
+_single underscores_
+
+**double asterisks**
+
+__double underscores__
+
+
+- And of course there are lists.
+- Some are bulleted.
+- With different indentation levels.
+        - That look like this.
+**bold **
+![alt text](image.jpg)
+
+
+`);
+  marked.setOptions({
+    breaks:true
+  })
+  const previewChange = (e) => { 
+setChange(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label for="editor" >Editor</label>
+      <textarea id="editor" onChange={(e) => { previewChange(e) }}
+        value={change}></textarea>
+      <label>previewer</label>
+      
+      <div id="preview"
+        dangerouslySetInnerHTML={{
+          __html:marked(change),
+        }}
+      ></div>
+
     </div>
   );
 }
